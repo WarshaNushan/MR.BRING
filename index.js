e85211ed70c2a39760cc12d9c830d0b0613fa166
WarshaNushan <129195361+WarshaNushan@users.noreply.github.com>
2024-08-26 18:32:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const util = require('util')
 const { sms,downloadMediaMessage } = require('./lib/msg')
 const axios = require('axios')
 const { File } = require('megajs')
-const prefix = '.'
+
 
 const ownerNumber = ['94704162270']
 
@@ -43,6 +43,11 @@ async function connectToWA() {
 const connectDB = require('./lib/mongodb')
 connectDB();
 //================================================
+const {readEnv} = require('./lib/database')
+const config = await readEnv();
+const prefix = config.PREFIX
+//===============================================
+        
 console.log("Connecting MR.BRING bot 🧬...");
 const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/')
 var { version } = await fetchLatestBaileysVersion()
