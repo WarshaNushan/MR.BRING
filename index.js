2e03196a46e7de607ffdcb254b6285316bc6df1a
WarshaNushan <129195361+WarshaNushan@users.noreply.github.com>
2024-08-26 18:25:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,6 +39,10 @@ const port = process.env.PORT || 8000;
 //=============================================
 
 async function connectToWA() {
+//================connect mongodb===================
+const connectDB = require('./lib/mongodb')
+connectDB();
+//================================================
 console.log("Connecting MR.BRING bot 🧬...");
 const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/')
 var { version } = await fetchLatestBaileysVersion()
