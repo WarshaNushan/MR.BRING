7c460ae2364e084cc2749f6f6fa08597cdde7dbe
WarshaNushan <129195361+WarshaNushan@users.noreply.github.com>
2024-08-26 17:13:14 +0530

    Create menu.js

---
+++ plugins/menu.js
@@ -0,0 +1,37 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+
+cmd({
+    pattern: "menu",
+    desc: "get cmd list",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let munu = {
+main: '',
+download: '',
+group: '',
+owner: '',
+convert: '',
+search: ''    
+};
+
+for (let i = 0; i < commands.length; i++) {
+if (commands[i].pattern && !commands[i].dontAddCommandList) {
+menu[commands[i].category] += `.${commands[i].pattern}\n`;
+ }
+}
+
+let madeMenu = ''
+
+
+  
+
+  
+}catch(e){
+console.log(e)
+reply('${e}')
+}
+  
