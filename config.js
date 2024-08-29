cf41f7c4f45379ef5ded92c4fcd9cf8349704534
WarshaNushan <129195361+WarshaNushan@users.noreply.github.com>
2024-08-29 18:27:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
-MONGODB: process.env.MONGODB || "mongodb+srv://warshanushan:<db_password>@cluster0.r94mt.mongodb.net/",
+SESSION_ID: process.env.SESSION_ID || "SESSION_ID here"
+MONGODB: process.env.MONGODB || "enter mongodb url",
 };
