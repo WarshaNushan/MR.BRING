0757c8cc436b95cd79e72212fdff46df0a0d4aba
WarshaNushan <129195361+WarshaNushan@users.noreply.github.com>
2024-09-02 15:40:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "SESSION_ID here"
-MONGODB: process.env.MONGODB || "enter mongodb url",
+SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/d6xsG7k/Presentation1-2.jpg"
+ALIVE_MSG: process.env.ALIVE_MSG || "HELLO, I AM MRBRING IAM ALIVE NOW💚"  
 };
